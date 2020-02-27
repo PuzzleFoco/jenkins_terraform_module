a5dd868894ef7295063edb5def7eedbd57425ce3
FabiusE <fabius.engel@msg.group>
2020-02-27 15:45:24 +0100

    Changed Version to String

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "helm_release" "jenkins"{
     name        = "jenkins"
     namespace   = kubernetes_namespace.jenkins_namespace.metadata[0].name
     chart       = "stable/jenkins"
-    version     = 1.9.19
+    version     = "1.9.19"
     timeout     = 600
 }
