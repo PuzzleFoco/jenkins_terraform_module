6cfcfcc270de55402cf74a8c02be06ccd6fd3b2d
FabiusE <fabius.engel@msg.group>
2020-02-27 15:43:09 +0100

    Creates Namespace for jenkins

--- example/main.tf
+++ example/main.tf
@@ -9,14 +9,14 @@
  */
 
 provider "helm" {
-    version        = "~> 0.9"
-    install_tiller = true
+    version        = "~> 1.0.0"
 
     kubernetes {
       host                   = module.k8s.host
       client_certificate     = base64decode(module.k8s.client_certificate)
       client_key             = base64decode(module.k8s.client_key)
       cluster_ca_certificate = base64decode(module.k8s.cluster_ca_certificate)
+      load_config_file       = false
   }
 }
 
--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
+resource "kubernetes_namespace" "jenkins_namespace" {
+    metadata {
+        annotations = {
+            name = "jenkins"
+        }
+        name = "jenkins"
+    }
+}
+
 resource "helm_release" "jenkins"{
     name        = "jenkins"
-    namespace   = "jenkins"
+    namespace   = kubernetes_namespace.jenkins_namespace.metadata[0].name
     chart       = "stable/jenkins"
+    version     = 1.9.19
     timeout     = 600
 }
